f11ad06020adc517bb09f52f4fcf01d3ce1dc9cd
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-07-25 20:50:33 -0600

    Add method ReverseString and rename ValidateMinMaxLengthString and ValidateSplitCount.

--- Framework.Utilities2023/UtilitiesString.cs
+++ Framework.Utilities2023/UtilitiesString.cs
@@ -9,14 +9,23 @@ namespace Framework.Utilities2023.UtilitiesString
     public static class UtilitiesString
     {
 
-        public static bool Validate_Min_Max_Length_String(this string str, int min, int max)
+        public static bool ValidateMinMaxLengthString(this string str, int min, int max)
         => str.Length >= min && str.Length <= max;
 
-        public static bool Validate_Split_Count(this string str, int count, char[] chare)
+        public static bool ValidateSplitCount(this string str, int count, char[] chare)
         {
             string[] result = str.Split(chare);
 
             return result.Length == count;
+        }
+
+        public static string ReverseString(this string str)
+        {
+            char[] convert = str.ToCharArray();
+
+            Array.Reverse(convert);
+
+            return new string(convert);
         }
 
         
