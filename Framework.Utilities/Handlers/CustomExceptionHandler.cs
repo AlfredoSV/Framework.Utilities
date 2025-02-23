f8046170f0db351e482949b5f56967d2fe52f6d1
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2025-02-23 09:29:54 -0600

    Added CustomExceptionHandler for web apps and web apis.

---
+++ Framework.Utilities/Handlers/CustomExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using Framework.Utilities.IServices;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace Framework.Utilities.Email.Handlers;
+
+public class CustomExceptionHandler : IExceptionHandler
+{
+    private readonly IServiceLogBook serviceLogBook;
+
+    public CustomExceptionHandler(IServiceLogBook serviceLogBook)
+    {
+        this.serviceLogBook = serviceLogBook;
+    }
+    public  ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var error = exception;
+
+        this.serviceLogBook.SaveErrorLog(error);
+
+        return ValueTask.FromResult(true);
+    }
+}
